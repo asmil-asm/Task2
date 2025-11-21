ebaf5f129f64948529a07fe259c18f431259ebf0
Asmail <asmil9069@gmail.com>
2025-11-21 15:46:18 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter as Router, Routes,Route,Navigate } from 'react-router-dom';
+import {HashRouter as Router, Routes,Route,Navigate } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
 import Admin from './components/Admin';
